39bb2cd7f2e973b78b8893b127fc4ea5a14bbdc9
Dominika Indrova <dominika.indrova@gmail.com>
2024-08-10 15:07:19 +0200

    Initial commit

---
+++ app/src/main/java/com/example/taskmanager/MainActivity.kt
@@ -0,0 +1,91 @@
+package com.example.taskmanager
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.AbsoluteAlignment
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.taskmanager.ui.theme.TaskManagerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TaskManagerTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    WholePage(
+                        firstText = stringResource(R.string.First_Text),
+                        secondText = stringResource(R.string.Second_Text),
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun WholePage(firstText: String, secondText: String, modifier: Modifier = Modifier) {
+    val image = painterResource(id = R.drawable.ic_task_completed)
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        Image(
+            //Image centered vertically and horizontally.
+            painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.Inside,
+        )
+        Text(
+            //First text.
+            text = firstText,
+            textAlign = TextAlign.Center,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .padding(
+                    top = 24.dp,
+                    bottom = 8.dp
+                )
+        )
+        Text(
+            //Second text.
+            text = secondText,
+            fontSize = 16.sp,
+            textAlign = TextAlign.Justify,
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    TaskManagerTheme {
+        WholePage(
+            firstText = stringResource(R.string.First_Text),
+            secondText = stringResource(R.string.Second_Text)
+        )
+    }
+}
